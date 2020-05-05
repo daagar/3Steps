24fb1b6571c29c6d617f4d0a4dc18cea7ff871ea
will <1-will@users.noreply.git.testlab.local>
2020-05-05 06:47:11 -0400

    added yum install to update-tintin.sh

--- update-tintin.sh
+++ update-tintin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 vers=2.02.0
 subvers=2.02.02
+yum -y install make gcc zlib-devel pcre-devel
 wget https://downloads.sf.net/tintin/tintin-${subvers}.tar.gz
 tar xvfz tintin-${subvers}.tar.gz
 cd tt/src
