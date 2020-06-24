65c02613cc92f3e5c47da18ae733f35c750d2e2b
Will <will@testlab.local>
2020-06-24 06:20:07 -0500

    Minor improvements to go alias to define a safe room

--- update-tintin.sh
+++ update-tintin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 vers=2.02.0
-subvers=2.02.02
+subvers=2.02.03
 sudo yum -y install make gcc zlib-devel pcre-devel wget
 sudo mkdir -pm777  /tintin/logs
 wget https://downloads.sf.net/tintin/tintin-${subvers}.tar.gz
